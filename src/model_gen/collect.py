b66d130320111d2708fbe2c5123d67ec36221930
Krishna Bhatt <krishbhatt2019@gmail.com>
2025-06-30 15:43:40 -0700

    Update 6/30/25

--- src/model_gen/collect.py
+++
@@ -1,7 +0,0 @@
-'''
-Generating Models of Evolution for Simulation
-
-Each model can be classified into three general clades: bacteria, protein, vertebrates.
-Model Parameters include: Clade, BD rates, coalescent rates, mixing partitions, sequence length, number of taxa, GTR alignment parameter set, indel rates, gap distribution
-'''
-
